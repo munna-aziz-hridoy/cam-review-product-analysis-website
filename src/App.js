abb567a8f6701f5c78e64ae7732384832990f67b
munna-aziz-hridoy <munna.aziz.hridoy@gmail.com>
2022-04-04 06:39:08 +0800

    blog section initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blogs from "./Components/Blogs/Blogs";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Error404 from "./Components/Error404/Error404";
 import Header from "./Components/Header/Header";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/review" element={<Review />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
